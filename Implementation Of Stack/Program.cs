894c5e91e3c2e76aaa9493107d186414fba299ef
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-09-25 14:52:31 +0530

    Add files via upload
    Stack Implementation for implementing Inheritance

---
+++ Implementation Of Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImplementationOfStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+     
+            
+            var stack = new Stack();
+
+            stack.Push(123);
+            stack.Push(124);
+            stack.Push(125);
+            stack.Push(null);
+
+            object j =  stack.Pop();
+            object j1 = stack.Pop();
+            Console.WriteLine($"Popped Element: {j}");
+            Console.WriteLine($"Popped Element: {j1}");
+
+            stack.Display();
+        }
+    }
+}
---
+++ Implementation Of Stack/Stack.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+namespace ImplementationOfStack
+{
+    public class Stack
+    {
+
+        private int _top;
+        private readonly List<object> list = new List<object>();
+
+
+        public void Push(object o)
+        {
+                
+            if (o == null)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                _top++;
+                list.Add(o);
+            }
+            
+           
+            
+        }
+
+        public object Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                object l = list[_top - 1];
+                list.RemoveAt(_top - 1);
+                _top--;
+                return l;
+            }
+        }        
+
+
+        public void Clear()
+        {
+            list.Clear();
+            System.Console.WriteLine("Remove Successfully");
+        }
+        public void Display()
+        {
+            System.Console.WriteLine("Stack Elements: ");
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
