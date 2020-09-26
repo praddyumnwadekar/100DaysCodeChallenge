54e5c4bff77e9e32f0b7d2c191839648e122700b
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-09-26 23:35:00 +0530

    Codes Uploaded

---
+++ StackOverFlowPost/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackOverFlowPost
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var listOfPost = new List<StackOverFlow>();
+
+            listOfPost.Add(new StackOverFlow()
+            {
+                Title = "Post Of Recurssion",
+                Description = "Just Showing the Working of the recurssion methods",
+                Date = new DateTime(2019, 7, 12)
+            }
+            );
+            
+            listOfPost.Add(new StackOverFlow()
+            {
+                Title = "Post Of Tree",
+                Description = "Just Showing the Working of the Tree Data Structure",
+                Date = new DateTime(2018, 3, 1)
+            }
+            );
+
+            listOfPost[0].Up_vote();
+            listOfPost[0].Up_vote();
+            listOfPost[0].Up_vote();
+            listOfPost[0].Up_vote();
+
+
+            listOfPost[1].Up_vote();
+            listOfPost[1].Up_vote();
+            listOfPost[1].Up_vote();
+            listOfPost[1].Up_vote();
+            listOfPost[1].Up_vote();
+            listOfPost[1].Up_vote();
+
+            listOfPost[0].Down_vote();
+            listOfPost[0].Down_vote();
+
+
+            listOfPost[1].Down_vote();
+            listOfPost[1].Down_vote();
+            listOfPost[1].Down_vote();
+            
+
+            foreach (var post in listOfPost)
+            {
+                Console.WriteLine($"Title : {post.Title}\n Description: {post.Description}\n Date Of Post: {post.Date}");
+                Console.WriteLine($"Votes : {post.GetUp_vote}");
+                Console.WriteLine();
+            }         
+           
+        }
+    }
+}
---
+++ StackOverFlowPost/StackOverFlow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StackOverFlowPost
+{
+    class StackOverFlow
+    {
+        private  int _vote;
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        
+        public void Up_vote()
+        {
+            this._vote++;
+        }
+
+        public void Down_vote()
+        {
+            this._vote--;
+        }
+
+        public int GetUp_vote
+        {
+            get 
+            {
+                return this._vote;
+            }
+
+        }
+
+        
+    }
+}
