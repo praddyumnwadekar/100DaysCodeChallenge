70e98a898ea881eba4523cdc840a4b292d03356a
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-09-28 22:55:51 +0530

    Add files via upload
    FaceBook Post it is a program about taking input of post which should not be null. which shows how many people like you post, in different way.
    
    Like below:
    
       1 - if you post like by one person , then it shows name of that person.
        2- if you post like by two person , then it shows name of 1st person  and other.
       3 - if you post like by 3 or more than that person , then it shows name first two person and remaining count.

---
+++ FaceBookPost/ExerciseFB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoopControls
+{
+    class ExerciseFB
+    {
+        public static void Main()
+        {
+           List<string> names = new List<string>();
+           int count = 0;
+           string name;
+
+           while (true)
+           {
+               name = Console.ReadLine();
+                
+               if (name.Equals(""))
+                   break;
+                
+               names.Add(name);
+           }
+           if (names.Count > 3)
+           {
+               count = 3;
+           } 
+           switch (count) {
+
+               case 0:
+                   break;
+               case 1:
+                   Console.WriteLine("{0} Likes your post", names[0]);
+                   break;
+               case 2:
+                   Console.WriteLine("{0} and {1} Likes your post", names[0],names[1]);
+                   break;
+               case 3:
+                   Console.WriteLine("{0},{1} and {2}", names[0], names[1], (names.Count) - 2);
+                   break;
+
+           }
+        }
+    }
+}
