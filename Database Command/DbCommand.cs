4671b0a5ccecc39ff3e5514fd68b082437597ec5
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-10-01 23:03:29 +0530

    Add files via upload

---
+++ Database Command/DbCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Database_Connection
+{   
+    public class DbCommand
+    {
+        private string _command;
+        private DatabaseConnection _databaseConnection;
+        public DbCommand(string command,DatabaseConnection databaseConnection)
+        {
+            if(databaseConnection == null)
+            {
+                throw new NullReferenceException("DatabaseConnection is Required");
+            }
+            
+            if(string.IsNullOrEmpty(command))
+            {
+                throw new ArgumentNullException("Command is Required");
+            }
+
+            this._command = command;
+            this._databaseConnection = databaseConnection; 
+        }
+        
+        private void Run()
+        {
+            Console.WriteLine($"The Command: \"{_command}\" run successfully.");
+        }
+        public void Execute()
+        {
+            _databaseConnection.Opening();
+            Run();
+            _databaseConnection.Closing();
+        }
+    }
+}
---
+++ Database Command/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Database_Connection
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var sqC = new SqlConnection("Sql Connection", new TimeSpan(0, 0, 5));                       // Sql Connection
+                sqC.Opening();
+                sqC.Closing();
+
+            //    var Orc = new OracleConnection("Oracle Connection", new TimeSpan(0, 0, 16));                // Oracle Connection.
+              //  Orc.Opening();
+                //Orc.Closing();
+                
+                DbCommand dbCommand = new DbCommand("select * from Students", sqC);
+                dbCommand.Execute();
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+         
+        }
+    }
+}
