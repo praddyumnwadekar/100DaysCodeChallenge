e9c824f0a3638406f2087a46c4bb8212b68dfdb2
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-10-03 09:58:53 +0530

    Update ShowDetails.cs

--- Opps Concepts/ShowDetails.cs
+++ Opps Concepts/ShowDetails.cs
@@ -5,9 +5,8 @@ using System.Text;
 namespace OppsConcept
 {
     class ShowDetails
-    {
-        public ShowDetails(IPerson I)
-        {
+    {                                   
+        public ShowDetails(IPerson I)               // Loosely coupled.        {
             I.Show();
         }
     }
