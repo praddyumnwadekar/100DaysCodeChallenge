698c5ee095b28ee6d37b248c7a4dd2d3e39c4942
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-10-05 18:33:40 +0530

    Add files via upload
    Finding the , Numbers in the list are in the consecutive manner or not

---
+++ FindingConsecutiveNumbers/ExerciseConsecutive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoopControls
+{
+    class ExerciseConsecutive
+    {
+        public static void Main()
+        {
+           Console.WriteLine("ENTER THE NUMBER IN (1-2-3-4-5) THIS FORMAT(HYPHEN)");
+           var input= Console.ReadLine();
+
+           List<int> numbers = new List<int>();
+
+           foreach (var number in input.Split('-'))
+               numbers.Add(Convert.ToInt32(number));
+            
+          numbers.Sort();
+
+           bool isConsecutive = true;
+           for (int i = 0; i < numbers.Count; i++)
+           { 
+               if(numbers[i] != numbers[i - 1] + 1)
+               {
+                   isConsecutive = false;
+                   break;
+               }
+           }
+
+           Console.WriteLine(isConsecutive ? "Consecutive" : "Non Consecutive");
+        }
+    }
+}
