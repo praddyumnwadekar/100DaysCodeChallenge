69ce613375efc98a7ec287dd8b7b935b22877d12
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-10-06 21:39:21 +0530

    Add files via upload
    Pascal Casing is the program where we check the user input  string converted into the pascal casing.

---
+++ Pascal Casing/ExerciseUserInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoopControls
+{
+    class ExerciseUserInput
+    {
+        public static void Main()
+        {
+            PascalCasing ps = new PascalCasing();
+            ps.ConvertStringIntoPascalCasing("NUMBER OF STUDENTS");
+        }  
+    }
+}
---
+++ Pascal Casing/PascalCasing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoopControls
+{
+    class PascalCasing
+    {
+        public void ConvertStringIntoPascalCasing(string normal)
+        {
+            StringBuilder pascalString = new StringBuilder();
+
+            foreach (var item in normal.Split(' '))
+            {
+                string s = item.Substring(0,1);
+                pascalString.Append(s.ToUpper())
+                            .Append(item.Substring(1).ToLower());
+            }
+            Console.WriteLine($"Pascal String:  {pascalString}");
+        }
+    }
+}
