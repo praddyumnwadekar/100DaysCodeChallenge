19a5ec3f96a527211b00b1155605d14907ab28b9
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-10-07 22:15:00 +0530

    Add files via upload
    Counting vowels : Count the number of vowels contain in the string.

---
+++ CountingVowels/CountiningVowels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoopControls
+{
+    class CountiningVowels
+    {
+        public void  CountingTheNumbersOfVowels(string name) 
+        {
+            int count = 0;
+            //string vowels = "aeiou";
+
+            foreach (var item in name.ToLower())
+            {
+                if(item == 'a' || item == 'e'|| item == 'i' || item == 'o' || item == 'u') 
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Vowels Count: {count}");
+        }
+    }
+}
