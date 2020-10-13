9850803697721d5eca9858cc550967065c290950
praddyumnwadekar <55682027+praddyumnwadekar@users.noreply.github.com>
2020-10-13 21:25:05 +0530

    Add files via upload
    Conversion between Celsius to kelvin and Fahrenheit

---
+++ DegreeConverter/CelsiusDegreeConverter.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace DegreeConverter
+{
+    public class CelsiusDegreeConverter
+    {
+        private float celsius;
+
+        public float Celsius
+        {
+            set
+            {
+                celsius = value;
+
+            }
+        }
+
+        public void ConvertDegreeToKelvin()
+        {
+            Console.WriteLine("The conversion of celsius {0} is = {1}", celsius, (celsius + 273));
+        }
+
+        public void ConvertDegreeToFahrenheit()
+        {
+            Console.WriteLine("The conversion of celsius {0} is = {1}", celsius, (celsius * 9 / 5) + 32);
+        }
+
+    }
+}
---
+++ DegreeConverter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DegreeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dc = new CelsiusDegreeConverter();
+
+            Console.WriteLine("Enter the Degree Celsius");
+            //int celsiusDegree = Convert.ToInt32(Console.ReadLine());
+            dc.Celsius = 1;
+            dc.ConvertDegreeToKelvin();
+            dc.ConvertDegreeToFahrenheit();
+        }
+    }
+}
